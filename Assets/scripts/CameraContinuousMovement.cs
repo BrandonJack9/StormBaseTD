6f573d06c6c4bc4c3555cb3c5efbe7b91e211f40
jwvonk <jwavonk@gmail.com>
2024-09-10 02:10:09 -0700

    Camera script updates position in edit mode

--- Assets/scripts/CameraContinuousMovement.cs
+++ Assets/scripts/CameraContinuousMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal.Internal;
 
+[ExecuteInEditMode]
 public class CameraContinuousMovement : MonoBehaviour
 {
     public Transform orbitCenter;
@@ -12,31 +13,21 @@ public class CameraContinuousMovement : MonoBehaviour
 
     void Start()
     {
-        transform.position = orbitCenter.position + new Vector3(0, orbitHeight, -orbitRadius);
-        transform.LookAt(orbitCenter);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Quaternion.AngleAxis(orbitSpeed * (-Input.GetAxis("Horizontal")) * Time.deltaTime, orbitCenter.up) * transform.position;
-        transform.LookAt(orbitCenter);
-    }
-
-    void OnDrawGizmos()
-    {
-        if (orbitCenter != null)
+        if (!Application.isPlaying)
         {
-            var startPos = orbitCenter.position + new Vector3(0, orbitHeight, -orbitRadius);
-
-            // Draw the start position
-            Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(startPos, 1f);
-
-            // Draw a line between the orbit center and the start position
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawLine(orbitCenter.position, startPos);
-
+            transform.position = orbitCenter.position + new Vector3(0, orbitHeight, -orbitRadius);
+            transform.LookAt(orbitCenter);
+        } else
+        {
+            transform.position = Quaternion.AngleAxis(orbitSpeed * (-Input.GetAxis("Horizontal")) * Time.deltaTime, orbitCenter.up) * transform.position;
+            transform.LookAt(orbitCenter);
         }
+
     }
 }

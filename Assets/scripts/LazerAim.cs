cbc0fb886ff00dbc7e58d565504db008821e852d
Brayden Smith <bsmit104@ucsc.edu>
2024-09-11 10:39:24 -0700

    heartBar

--- Assets/scripts/LazerAim.cs
+++ Assets/scripts/LazerAim.cs
@@ -22,40 +22,15 @@ public class LazerAim : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 500, layersToHit))
         {
-<<<<<<< HEAD
-            HandeLazerFire(hit);
-=======
             //GameObject currentLazer = Instantiate(lazerPrefab);
             //currentLazer.transform.position = barrelPoint.transform.position;
             //currentLazer.transform.up = barrelPoint.transform.forward;
             barrelPoint.transform.LookAt(hit.point);
             gunBarrel.transform.LookAt(hit.point);
             Debug.DrawRay(barrelPoint.transform.position, barrelPoint.forward * 500, Color.red);
->>>>>>> parent of a85712e (added lazer support)
         }
        
         
         
-<<<<<<< HEAD
-    }
-
-    private void HandeLazerFire(RaycastHit hit){
-        GameObject hitObject = hit.collider.gameObject;
-            if (hitObject != null){
-                 if (lastUsedTime + lazerDelay < Time.time){
-                    GameObject currentLazer = Instantiate(lazerPrefab);
-                    currentLazer.transform.position = barrelPoint.transform.position;
-                    currentLazer.transform.up = barrelPoint.transform.forward;
-                    while(currentLazer.transform.position != hitObject.transform.position){
-                        currentLazer.transform.position = Vector3.MoveTowards(currentLazer.transform.position, hitObject.transform.position, lazerSpeed);
-                    }
-                    lastUsedTime = Time.time;
-                 }
-                barrelPoint.transform.LookAt(hit.point);
-                gunBarrel.transform.LookAt(hit.point);
-                Debug.DrawRay(barrelPoint.transform.position, barrelPoint.forward * 500, Color.red);
-            }
-=======
->>>>>>> parent of a85712e (added lazer support)
     }
 }

db2d148b2b4e8440e5909b2442bd8b22e8843879
BrandonJack9 <bjack@ucsc.edu>
2024-09-11 23:10:40 -0700

    Revert "refactored"
    This reverts commit 309ea48edcf55b21e801473f0d4ebc9274769ed8.

--- Assets/scripts/LazerAim.cs
+++ Assets/scripts/LazerAim.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,6 +8,10 @@ public class LazerAim : MonoBehaviour
 {
     public Transform gunBarrel, aimLookAt, barrelPoint, cameraPoint;
     public GameObject lazerPrefab;
+    public float lazerSpeed = 0.5f;
+
+    public float lazerDelay = 5;
+    private float lastUsedTime;
     public LayerMask layersToHit;
     //public float projectileLifeTime, shootForce;
     // Start is called before the first frame update
@@ -22,25 +27,7 @@ public class LazerAim : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 500, layersToHit))
         {
-<<<<<<< HEAD
-            HandeLazerFire(hit);
-=======
-            //GameObject currentLazer = Instantiate(lazerPrefab);
-            //currentLazer.transform.position = barrelPoint.transform.position;
-            //currentLazer.transform.up = barrelPoint.transform.forward;
-            barrelPoint.transform.LookAt(hit.point);
-            gunBarrel.transform.LookAt(hit.point);
-            Debug.DrawRay(barrelPoint.transform.position, barrelPoint.forward * 500, Color.red);
->>>>>>> parent of a85712e (added lazer support)
-        }
-       
-        
-        
-<<<<<<< HEAD
-    }
-
-    private void HandeLazerFire(RaycastHit hit){
-        GameObject hitObject = hit.collider.gameObject;
+            GameObject hitObject = hit.collider.gameObject;
             if (hitObject != null){
                  if (lastUsedTime + lazerDelay < Time.time){
                     GameObject currentLazer = Instantiate(lazerPrefab);
@@ -55,7 +42,10 @@ public class LazerAim : MonoBehaviour
                 gunBarrel.transform.LookAt(hit.point);
                 Debug.DrawRay(barrelPoint.transform.position, barrelPoint.forward * 500, Color.red);
             }
-=======
->>>>>>> parent of a85712e (added lazer support)
+            
+        }
+       
+        
+        
     }
 }

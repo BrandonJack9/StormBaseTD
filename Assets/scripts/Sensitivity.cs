8da3c059aa2905165ee476508ffe6d82a78f2bfa
JarlRamos2019 <111897535+JarlRamos2019@users.noreply.github.com>
2024-09-12 16:14:01 -0700

    Reimplemented the sensitivity controls

---
+++ Assets/scripts/Sensitivity.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Sensitivity : MonoBehaviour
+{
+    public Slider sensSlider;          // sensitivity slider in Settings panel
+    public TextMeshProUGUI sensNumber; // sensitivity ranking (default: 1 to 4)
+    public int sensThresholdX;         // how much the x-axis sensitivity will change with slider
+    public int sensThresholdY;         // how much the y-axis sensitivity will change with slider  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set the default camera speed to level corresponding to sensNumber = 1
+        Cinemachine.CinemachineFreeLook mCameraCM = Camera.main.GetComponent<Cinemachine.CinemachineFreeLook>();
+        mCameraCM.m_XAxis.m_MaxSpeed = sensThresholdX;
+        mCameraCM.m_YAxis.m_MaxSpeed = sensThresholdY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeSensitivity();
+    }
+
+    // gets the slider value from Settings and modifies the Cinemachine camera speed
+    public void ChangeSensitivity()
+    {
+        // display the number on settings panel
+        sensNumber.text = sensSlider.value.ToString();
+        // set the Cinemachine speed by multiplying the slider value with the threshold
+        Cinemachine.CinemachineFreeLook mCameraCM = Camera.main.GetComponent<Cinemachine.CinemachineFreeLook>();
+        mCameraCM.m_XAxis.m_MaxSpeed = sensSlider.value * sensThresholdX;
+        mCameraCM.m_YAxis.m_MaxSpeed = sensSlider.value * sensThresholdY;
+    }
+}

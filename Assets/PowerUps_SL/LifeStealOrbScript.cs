d2f28c5639d9b476ff59cc556203d091cb5279bf
BrandonJack9 <bjack@ucsc.edu>
2024-09-12 21:47:41 -0700

    fixed life steal orb to work with gravity

--- Assets/PowerUps_SL/LifeStealOrbScript.cs
+++ Assets/PowerUps_SL/LifeStealOrbScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class LifeStealOrbScript : MonoBehaviour
 {
-    private Transform target;               // Target to move towards
     public float speed = 10f;                // Speed of the projectile
     public float rotateSpeed = 200f;        // Optional: Rotation speed for spinning meteor effect
     public GameObject impactEffect;         // Effect to spawn on impact
@@ -22,34 +21,14 @@ public class LifeStealOrbScript : MonoBehaviour
             }
             Destroy(gameObject);
         }
+
     }
     void Update()
     {
-        // if (stopAllMeteors)
-        // {
-        //     Destroy(gameObject);
-        //     return;
-        // }
-        // // Move the projectile if there's a valid target
-        // if (target != null)
-        // {
-        //     // Direction to the target
-        //     Vector3 direction = target.position - transform.position;
-        //     float distanceThisFrame = speed * Time.deltaTime;
-
-
-        //     transform.Translate(direction.normalized * distanceThisFrame, Space.World);
-
-        //     // Optional: Rotate the projectile for a meteor-like spinning effect
-        //     transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
-        // }
-
+      
     }
 
-    // Set the target for this projectile
-    public void SetTarget(Transform target)
-    {
-        // this.target = target;
-    }
+   
+  
 
 }

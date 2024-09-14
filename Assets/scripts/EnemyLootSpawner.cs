71ba12047d9aa22455b3888be57e8018c0370e78
BrandonJack9 <bjack@ucsc.edu>
2024-09-14 02:16:05 -0700

    Powerups now drop from dead enemies (Thank you Sam!!!)
    I just implemented a script made by sam for this push. He deservers all the credit for this one.

---
+++ Assets/scripts/EnemyLootSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLootSpawner : MonoBehaviour
+
+{
+    // Array to hold different loot prefabs
+    public GameObject[] lootPrefabs;
+
+    // Optional spawn offset
+    public Vector3 spawnOffset;
+
+    public float dropChance = 5f;
+
+    // OnDestroy drops powerup based on random roll
+    private void OnDestroy()
+    {
+        float randomRoll = Random.Range(0f, 100f);
+        if (randomRoll <= dropChance)
+        {
+            if (lootPrefabs.Length == 0)
+            {
+                Debug.LogWarning("No loot prefabs assigned.");
+                return;
+            }
+
+            // Choose a random loot prefab
+            int randomIndex = Random.Range(0, lootPrefabs.Length);
+            GameObject selectedLoot = lootPrefabs[randomIndex];
+
+            Instantiate(selectedLoot, transform.position + spawnOffset, Quaternion.identity);
+        }
+    }
+}
